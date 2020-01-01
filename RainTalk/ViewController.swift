53c89a9d1979cc7999269831158afc887e9671eb
Jinu <binaryrain01@gmail.com>
2020-01-01 22:21:05 +0900

    Initial Commit

---
+++ RainTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RainTalk
+//
+//  Created by Jinu on 2020/01/01.
+//  Copyright © 2020 Jinu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
