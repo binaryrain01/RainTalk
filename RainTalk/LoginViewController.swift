3290f2e150446108e0610c23c332b6b5c55064b1
binaryrain01 <binaryrain01@gmail.com>
2020-01-10 21:00:32 +0900

    add new file 'LoginViewController.swift'

--- RainTalk/LoginViewController.swift
+++ RainTalk/LoginViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signinButton: UIButton!
+    
+    let rc = RemoteConfig.remoteConfig()
+    var color: String! = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let statusBar = UIView()
+        self.view.addSubview(statusBar)
+        
+        statusBar.snp.makeConstraints { (m) in
+            m.left.top.right.equalTo(self.view)
+            m.height.equalTo(20)
+        }
+        
+        color = rc["splash_background"].stringValue
+        //print(color)
+        statusBar.backgroundColor = UIColor(hexString: color)
+        loginButton.backgroundColor = UIColor(hexString: color)
+        signinButton.backgroundColor = UIColor(hexString: color)
         // Do any additional setup after loading the view.
     }
     
